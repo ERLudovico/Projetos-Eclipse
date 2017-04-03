b95906ed057b4f9d82f61ea808349c6fbb196df1
Eduardo <erlludovico@gmail.com>
2017-04-03 10:10:59 -0300

    INcluindo Projeto Inicial de Teste de comunicacao com o Arduino

---
+++ Arduino/Teste Serial Arduino/build/.default/-.d
@@ -0,0 +1,2 @@
+.sc734680615928416069.o: \
+ E:\DADOS\ EXT\Programacao\Projetos-Eclipse\Arduino\Teste\ Serial\ Arduino\.sc734680615928416069.cpp
---
+++ Arduino/Teste Serial Arduino/build/Arduino UNO/-.d
@@ -0,0 +1,2 @@
+.sc2230031148336659497.o: \
+ E:\DADOS\ EXT\Programacao\Projetos-Eclipse\Arduino\Teste\ Serial\ Arduino\.sc2230031148336659497.cpp
