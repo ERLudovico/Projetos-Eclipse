4b6d36c281bb888377fd748be52a596d48ba6e69
Eduardo <erlludovico@gmail.com>
2018-01-07 14:12:05 -0200

    Incluindo a leitura serial

--- Arduino/Telemetria/V2/TelemetriaRX/src/main.cpp
+++ Arduino/Telemetria/V2/TelemetriaRX/src/main.cpp
@@ -1,14 +1,130 @@
 #include <Arduino.h>
 
+
+
+void parseData();
+void showParsedData();
+void recvWithStartEndMarkers();
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+      // variables to hold the parsed data
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+float floatFromPC = 0.0;
+
+boolean newData = false;
+
+int qtCell = 0 ;
+float voltCell[] = { 0.00, 0.00, 0.00 ,0.00};
+float voltCellT = 0.00;
+
+
+//============
+
 void setup() {
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("This demo expects 3 pieces of data - text, an integer and a floating point value");
+    Serial.println("Enter data in this style <HelloWorld, 12, 24.7>  ");
+    Serial.println();
 }
 
+//============
+
 void loop() {
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(2000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(2000);              // wait for a second
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+            // this temporary copy is necessary to protect the original data
+            //   because strtok() used in parseData() replaces the commas with \0
+        parseData();
+        showParsedData();
+        newData = false;
+    }
+}
+
+//============
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+//============
+
+void parseData() {      // split the data into its parts
+
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    qtCell = atoi(strtokIndx); // copy it to messageFromPC
+
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    voltCell[1] = atof(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    voltCell[2] = atof(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    voltCell[3] = atof(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    voltCellT = atof(strtokIndx);     // convert this part to an integer
+
+/*
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    integerFromPC = atoi(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ",");
+    floatFromPC = atof(strtokIndx);     // convert this part to a float
+*/
+
+}
+
+//============
+
+void showParsedData() {
+    Serial.print("Message ");
+    Serial.println(qtCell);
+    Serial.print("Vol 1: ");
+    Serial.println(voltCell[1]);
+    Serial.print("Vol 2: ");
+    Serial.println(voltCell[2]);
+    Serial.print("Vol 3: ");
+    Serial.println(voltCell[3]);
+    Serial.print("Vol Total: ");
+    Serial.println(voltCellT);
+
 }
