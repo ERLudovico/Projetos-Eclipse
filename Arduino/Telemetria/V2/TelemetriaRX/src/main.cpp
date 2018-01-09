6470ef99901b6bb2b256ad44c50de72d9f5a5472
Eduardo <erlludovico@gmail.com>
2018-01-09 15:14:24 -0200

    Incluindo OLED primeira versão

--- Arduino/Telemetria/V2/TelemetriaRX/src/main.cpp
+++ Arduino/Telemetria/V2/TelemetriaRX/src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define _DELAY_ 250000
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
+void checkTimet();
 void blickLED(int);
 void parseData();
 void debugParsedData();
+void displayOLEDData();
 void validaComunicacao();
 void recvWithStartEndMarkers();
+void generateMockValues();
 
 const byte numChars = 32;
 char receivedChars[numChars];
@@ -18,25 +28,26 @@ boolean newData = false;
 boolean ledRXLOW = true ;
 
 int qtCell = 0 ;
-float voltCell[] = { 0.00, 0.00, 0.00 ,0.00};
+float voltCell[]    = { 0.00, 0.00, 0.00 ,  0.00 };
+float voltPackMin[] = { 0.00, 3.80, 7.60 , 11.40 };
+float voltPackMed[] = { 0.00, 4.00, 8.00 , 12.00 };
 float voltCellT = 0.00;
 
 static char CARAC_INICIO = '<' ;
 static char CARAC_FIM    = '>' ;
 
-
-
 float seno;
 int frequencia;
 
-
+boolean blinkON = false ;
 
 int comunFailedCount = 0 ;
-
-// Variaves do timet
 unsigned long timet;
-boolean enterFunction = true;
 unsigned long previousTime;
+
+boolean enterFunction = true;
+// Variaveis debug
+//boolean debugOLED = false;
 
 //============
 
@@ -44,29 +55,122 @@ void setup() {
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(9,OUTPUT);
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+    display.display();
 }
 
 //============
 
 void loop() {
-	timet = millis();
-	validaComunicacao();
-    recvWithStartEndMarkers();
-    //blickLED();
-    if (newData == true) {
-        blickLED(10);
-        strcpy(tempChars, receivedChars);
-        parseData();
-        debugParsedData();
-        newData = false;
+	//timet = micros();
+	//previousTime = timet;
+	//validaComunicacao();
+    //if (debugOLED) {
+    //	generateMockValues();
+    //} else {
+	recvWithStartEndMarkers();
+	if (newData == true) {
+		blickLED(10);
+		strcpy(tempChars, receivedChars);
+		parseData();
+		debugParsedData();
+		newData = false;
+	}
+    //}
+	displayOLEDData();
+    if ( enterFunction ){
+    	previousTime = timet;
+    	if (blinkON) {
+    		blinkON = false ;
+    	} else {
+       		blinkON = true ;
+    	}
     }
+    checkTimet();
 
 }
 
 //============
 
+//void generateMockValues(){
+//
+//}
+
+void checkTimet(){
+
+  if (timet - previousTime < _DELAY_){
+	  enterFunction = false;
+  }  else {
+	  enterFunction = true;
+  }
+}
+
+void displayOLEDData(){
+	  display.clearDisplay();
+
+	  display.setTextSize(1);
+	  display.setTextColor(WHITE);
+	  display.setCursor(0,0);
+	  display.print("  ");
+
+	  for (int c=1; c<=qtCell;c++){
+
+		  if ( voltCell[c] <= voltPackMin[1]){
+
+			  display.setTextColor(BLACK, WHITE);
+			  display.print(voltCell[c]);
+
+		  } else if ( voltCell[c] <= voltPackMed[1]){
+
+			  if (blinkON) {
+				  display.setTextColor(BLACK, WHITE);
+			  } else {
+				  display.setTextColor(WHITE);
+			  }
+			  display.print(voltCell[c]);
+		  } else {
+			  display.setTextColor(WHITE);
+			  display.print(voltCell[c]);
+		  }
+
+		  display.setTextColor(WHITE);
+		  if ( c <= ( qtCell - 1) ){ display.print(" | ");}
+	  }
+
+	  display.drawLine(0, 8, display.width(), 8, WHITE);
+
+	  display.setTextSize(3);
+	  display.setTextColor(WHITE);
+	  display.setCursor(0,10);
+	  display.print(" ");
+
+	  if ( voltCellT <= voltPackMin[qtCell]){
+
+		  display.setTextColor(BLACK, WHITE);
+		  display.print(voltCellT);
+
+	  } else if ( voltCellT <= voltPackMed[qtCell]){
+		  if (blinkON) {
+			  display.setTextColor(BLACK, WHITE);
+		  } else {
+			  display.setTextColor(WHITE);
+		  }
+		  display.print(voltCellT);
+	  } else {
+		  display.setTextColor(WHITE);
+		  display.print(voltCellT);
+	  }
+
+	  //display.print(voltCellT);
+
+	  display.display();
+
+}
+
 void validaComunicacao(){
-	if (comunFailedCount > 1000) {
+//	if (comunFailedCount > 1000) {
 //		Serial.print(comunFailedCount);
 //		tone(9,400);
 //		delay(500);
@@ -80,7 +184,7 @@ void validaComunicacao(){
 //		  tone(9,frequencia);
 //		  delay(2);
 //		}
-	}
+//	}
 }
 
 void recvWithStartEndMarkers() {
@@ -113,22 +217,8 @@ void recvWithStartEndMarkers() {
     }
 }
 
-int finalTime = 0;
-int agora = 0 ;
-
 void blickLED(int _TIME_){
 
-//	if (ledRXLOW == true){
-//		digitalWrite(LED_BUILTIN, HIGH);
-//		ledRXLOW = false;
-//		finalTime = 5000 + millis();
-//	} else {
-//		agora = millis();
-//		if ( agora >= finalTime){
-//			digitalWrite(LED_BUILTIN, LOW);
-//			ledRXLOW = true;
-//		}
-//	}
 	digitalWrite(LED_BUILTIN, HIGH);
 	delay(_TIME_);
 	digitalWrite(LED_BUILTIN, LOW);
@@ -172,7 +262,7 @@ void parseData() {      // split the data into its parts
 //============
 
 void debugParsedData() {
-	Serial.print("RX ");
+	Serial.print("RX");
     Serial.print(CARAC_INICIO);
     Serial.print(",");
     Serial.print(qtCell);
@@ -185,6 +275,4 @@ void debugParsedData() {
     Serial.print(",");
     Serial.print(voltCellT);
     Serial.println(CARAC_FIM);
-
-
 }
